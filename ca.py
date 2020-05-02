a3bdd7b69904090b3e0b9ab8478fd794d4d85023
Ingo Weigel <me19m022@technikum-wien.at>
2020-05-02 09:59:57 +0200

    new cell matrix, added init(), placeCell() and TODOs

--- ca.py
+++ ca.py
@@ -14,8 +14,8 @@ y = np.arange(0, 49, dy)
 X, Y = np.meshgrid(x, y)
 extent = np.min(X), np.max(X), np.min(Y), np.max(Y)
 
-rows = 49
-columns = 67
+rows = 49  # the resolution of the y-axis in a carthesian coordinate system
+columns = 67  # the resolution of the x-axis in a carthesian coordinate system
 image = plt.imread("heart.png")
 
 
@@ -28,12 +28,15 @@ class Cell:
         self.mV = mV
 
     def get_state(self):
-        print(self.state)
+        # print(self.state)
         return self.state;
 
 
 class Heart:
     heart = []
+    # initialise the cell matrix with cells that are "no heart cells"
+    # TODO: which paramters for the Cell() constructor?
+    cells = [[Cell(0,10) for y in range(rows)] for x in range(columns)]
 
     # define cell types
     heart_cell = Cell(1, 30)
@@ -47,6 +50,9 @@ class Heart:
 
     def __init__(self):  # constructor
         self.heart = self.init_heart()
+        # TODO: these are for test purposes only
+        print("The x dimension of cells is:", len(self.cells))
+        print("The y dimension of cells is:", len(self.cells[1]))
 
     def init_heart(self):
         matrix = []
@@ -57,19 +63,45 @@ class Heart:
                 if i == 13 and j >= 16 and j <= 17 or i >= 14 and i <= 15 and j >= 15 and j <= 17:
                     r.append(self.sinus_knot.get_state())
                 else:
-                    r.append(self.no_heart_cell.get_state())
+                    r.append(self.no_heart_cell.get_state()) # TODO: cells must be copied, not referenced
 
             matrix.append(r)  # add the rows filled with columns to existing matrix list
 
         return matrix
+
+    def init(self):
+        # TODO: Here, use self.placeCell() to create the heart cell matrix
+        return
+
+    def placeCell(self, cell, x, y):
+        # TODO: check x and y for validity (IndexError: list index out of range)
+        self.cells[x][y] = cell  # TODO: some of the cells must be "copied", not referenced!
+        return
+
+    def getState(self):
+        state = []
+        for i in range(int(columns)):  # loop as many times as variable rows
+            state.append([])
+            for j in range(int(rows)):
+                state[i].append(self.cells[i][j].get_state())
+        return state
+
+    def step(self):  # one step transits the heard simulation 10ms ahead
+        # step all (cells except sine-knot) according to their neighbours (Moore neighbourhood)
+        # TODO
+        # step for sine-knot
+        # TODO
+        return
 
 
 heart = Heart()
 
 # plt.clf() clears window
 
-print(heart.heart)
+#print(heart.heart)
 plt.imshow(image, extent=extent)
 plt.imshow(heart.heart, extent=extent, cmap="Reds", alpha= 0.7)
-plt.title("Cellular Automata")
+#plt.imshow(heart.getState(), extent=extent, cmap="Reds", alpha= 0.7)
+plt.title("Cellular Automata of the Heart")
+# TODO: remove axis labels
 plt.show()
