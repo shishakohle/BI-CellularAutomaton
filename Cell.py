86f44ccf863d39a0fff64bdaba28fcb1665c9b98
kerstinpegler <kerstin.pegler@a1.net>
2020-05-07 22:35:20 +0200

    Implemented own colormap

--- Cell.py
+++ Cell.py
@@ -15,25 +15,25 @@ class Celltype:
 class Cell:
     color_state_polarized = {  # aktivierbar
         Celltype.NO_HEART_CELL: 0,
-        Celltype.RIGHT_ATRIUM: 4,
-        Celltype.LEFT_ATRIUM: 3,
-        Celltype.SINUS_KNOT: 1,
-        Celltype.MYOKARD: 5,
-        Celltype.PURKINJE: 1,
-        Celltype.TAWARA: 2,
-        Celltype.HIS_BUNDLE: 3,
-        Celltype.AV_KNOT: 4
+        Celltype.RIGHT_ATRIUM: 1,
+        Celltype.LEFT_ATRIUM: 1,
+        Celltype.SINUS_KNOT: 3,
+        Celltype.MYOKARD: 1,
+        Celltype.PURKINJE: 11,
+        Celltype.TAWARA: 9,
+        Celltype.HIS_BUNDLE: 7,
+        Celltype.AV_KNOT: 5
     }
     color_state_depolarized = {  # aktiviert
         Celltype.NO_HEART_CELL: 0,
-        Celltype.RIGHT_ATRIUM: 4,
-        Celltype.LEFT_ATRIUM: 3,
-        Celltype.SINUS_KNOT: 1,
-        Celltype.MYOKARD: 5,
-        Celltype.PURKINJE: 1,
-        Celltype.TAWARA: 2,
-        Celltype.HIS_BUNDLE: 3,
-        Celltype.AV_KNOT: 4
+        Celltype.RIGHT_ATRIUM: 2,
+        Celltype.LEFT_ATRIUM: 2,
+        Celltype.SINUS_KNOT: 4,
+        Celltype.MYOKARD: 2,
+        Celltype.PURKINJE: 12,
+        Celltype.TAWARA: 10,
+        Celltype.HIS_BUNDLE: 8,
+        Celltype.AV_KNOT: 6
     }
 
     dauer_erregung = 200  # für alle Zellen gleich 20 Zeiteinheiten (200 ms)
--- main.py
+++ main.py
@@ -5,6 +5,13 @@ TODO: Some comments on this project.
 from Heart import Heart
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.colors as colors
+
+# colormap
+cmap = colors.ListedColormap(['#FFFFFF', '#E9967A','#8B0000', '#AB82FF', '#5D478B', '#A2CD5A', '#556B2F', '#87CEFA',
+                              '#00008B', '#FFFF00', '#8B8B00', '#FF8C00', '#8B4500'])
+boundaries = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+norm = colors.BoundaryNorm(boundaries, cmap.N, clip=True)
 
 
 # steps for animation 10 ms (Zyklus: 0 - 200ms + 300ms Pause = 500ms gesamter Zyklus)
@@ -32,7 +39,7 @@ heart = Heart()
 # plt.clf() clears window
 
 plt.imshow(image, extent=extent)
-plt.imshow(heart.heart, extent=extent, cmap="Reds", alpha= 0.7)
+plt.imshow(heart.heart, extent=extent, cmap=cmap, alpha= 0.7)
 #plt.imshow(heart.getState(), extent=extent, cmap="Reds", alpha= 0.7)
 plt.title("Cellular Automata of the Heart")
 # TODO: remove axis labels
