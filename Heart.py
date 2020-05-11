1c9ec6f65c9cd24361983ed7ca2ebcb5de774faa
Ingo Weigel <me19m022@technikum-wien.at>
2020-05-11 17:03:55 +0200

    First draft for Heart.step()

--- Heart.py
+++ Heart.py
@@ -1,8 +1,6 @@
-from Cell import Cell
-from Cell import Celltype
+from Cell import *
+# from Cell import Celltype
 import csv
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 
 class Heart:
@@ -37,7 +35,7 @@ class Heart:
                     r.append(right_atrium)
                 elif heart[i][j] == '2':
                     sinus_knot = Cell(Celltype.SINUS_KNOT)
-                    sinus_knot.trigger();  # sine knot immediately triggered.
+                    sinus_knot.trigger()  # sine knot immediately triggered.
                     r.append(sinus_knot)
                 elif heart[i][j] == '3':
                     av_knot = Cell(Celltype.AV_KNOT)
@@ -62,31 +60,9 @@ class Heart:
 
         return matrix
 
-    """
-    def placeCell(self, cell, x, y):
-        # TODO: check x and y for validity (IndexError: list index out of range)
-        self.cells[x][y] = cell  # TODO: some of the cells must be "copied", not referenced!
-        return
-    """
-
-    """
-    # TODO: Frage von Ingo: Benötigen wir diese funktion noch?
-    def getState(self):  # returns a matrix that indicates the current state of each heart cell
-        state = []
-        for i in range(int(self.columns)):  # loop as many times as variable rows
-            state.append([])
-            for j in range(int(self.rows)):
-                state[i].append(self.cells[i][j].get_state())
-        return state
-    """
-
     def step(self):  # one step transits the heart simulation 1 time step ahead
-        # step all (cells except sine-knot) according to their neighbours (Moore neighbourhood)
-        # TODO
-        # step for sine-knot
-        # TODO
         # comments Anna:
-        # 1) alle Sinusknotenzellen gleichzeitig im Zeitpunkt 0 auf getriggered
+        # 1) alle Sinusknotenzellen gleichzeitig im Zeitpunkt 0 auf getriggered -> geschieht in Heart.__init__()
         # 2) Vorhof (links und rechts) beginnt sich gleicheitig vom Sinusknoten aus zu depolarizen (isTriggered) und gibt Impuls an Nachbarzellen weiter
         #    2.1) Wenn meine Nachbarzelle depolarized ist und ich polarizable --> isTriggered
         # 3) Vorhof darf nicht His-Bündel aktivieren, sondern nur AV Knoten
@@ -94,9 +70,54 @@ class Heart:
         # 5) Normale Weitergabe an Nachbarzellen für Tawara und Purkinje
         # 6) Myokard darf sich erst anfangen lassen von Nachbarzellen zu aktivieren, wenn alle Purkinje Fasern auf depolarized (3) sind
 
-        for cellrow in self.heart:
-            for cell in cellrow:
-                cell.trigger()
-                cell.step()
-                # TODO will be continued as soon as the "no Objects of type Cell in heart matrix" issue is resolved
+        for row in range( len(self.heart) ):
+            for column in range( len(self.heart[row]) ):
 
+                if self.heart[row][column].celltype == Celltype.HIS_BUNDLE:
+                    # trigger if: (1) all AV knots are depolarized and (2) neighbourhood contains an depolarized cell
+                    if self.allDepolarized(Celltype.AV_KNOT) && self.aNeighbourIsDepolarized(row, column):
+                        self.heart[row][column].trigger()
+
+                elif self.heart[row][column].celltype == Celltype.MYOKARD:
+                    # trigger if all Purkinje Fibres are depolarized
+                    if self.allDepolarized(Celltype.PURKINJE):
+                        self.heart[row][column].trigger()
+
+                else:
+                    # trigger if neighbourhood contains an depolarized cell
+                    if self.aNeighbourIsDepolarized(row, column):
+                        self.heart[row][column].trigger()
+
+                self.heart[row][column].step()
+
+    def aNeighbourIsDepolarized(self, row, column):
+        aNeighbourIsDepolarized = False
+
+        # neighbourhood: Moore
+        north = row    - 1 if row    - 1 in range(0,1,len(self.heart))      else north = row
+        south = row    + 1 if row    + 1 in range(0,1,len(self.heart))      else south = row
+        east  = column + 1 if column + 1 in range(0,1,len(self.heart[row])) else east  = column
+        west  = column - 1 if column - 1 in range(0,1,len(self.heart[row])) else west  = column
+
+        # for all 8 neighbours: check if they are depolirated (if they exist)#
+        if self.heart[north][column].getState() == StateName.DEPOLARIZED: aNeighbourIsDepolarized = True
+        if self.heart[north][east]  .getState() == StateName.DEPOLARIZED: aNeighbourIsDepolarized = True
+        if self.heart[row]  [east]  .getState() == StateName.DEPOLARIZED: aNeighbourIsDepolarized = True
+        if self.heart[south][east]  .getState() == StateName.DEPOLARIZED: aNeighbourIsDepolarized = True
+        if self.heart[south][column].getState() == StateName.DEPOLARIZED: aNeighbourIsDepolarized = True
+        if self.heart[south][west]  .getState() == StateName.DEPOLARIZED: aNeighbourIsDepolarized = True
+        if self.heart[row]  [west]  .getState() == StateName.DEPOLARIZED: aNeighbourIsDepolarized = True
+        if self.heart[north][west]  .getState() == StateName.DEPOLARIZED: aNeighbourIsDepolarized = True
+
+        return aNeighbourIsDepolarized
+
+    def allDepolarized(self, cellytpe):
+        allDepolarized = True
+
+        for row in range(len(self.heart)):
+            for column in range(len(self.heart[row])):
+
+                if self.heart[row][column].celltype == cellytpe:
+                    if self.heart[row][column].getState() != StateName.DEPOLARIZED: allDepolarized = False
+
+        return allDepolarized
